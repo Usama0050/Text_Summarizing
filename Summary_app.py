418170528df5cdf107f3092d14774d24245ec2c0
Usama0050 <usamasadique176@gmail.com>
2025-05-29 17:57:34 +0500

    Update Summary_app.py

--- Summary_app.py
+++ Summary_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-# Load from Hugging Face Hub
 @st.cache_resource
 def load_model():
-    model_name = "Usamahf0050/t5_model"  # 🔁 Replace with your model name
+    model_name = "Usamahf0050/t5_model"
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name)
     return model, tokenizer
